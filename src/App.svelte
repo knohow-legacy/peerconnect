09df0e4c29cc3b3a2d3405e054752a2c63f20c02
cabalex <31020729+cabalex@users.noreply.github.com>
2023-01-07 09:23:29 -0800

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -57,7 +57,7 @@
 </script>
 
 <svelte:head>
-    <script src="https://accounts.google.com/gsi/peer" async defer />
+    <script src="https://accounts.google.com/gsi/client" async defer />
 </svelte:head>
 
 <Header />
